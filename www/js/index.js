74bba24ab843e12262564753a51a5cc50f45b9bb
Murilo Sardinha <murilosardinha@gmail.com>
2016-10-05 16:03:16 -0300

    removes plugins && adds materialize

--- www/js/index.js
+++ www/js/index.js
@@ -1,18 +1,19 @@
 var db = openDatabase('partiesdb', '1.0', 'my first database', 2 * 1024 * 1024);
+get_value_from_parties();
 
 db.transaction(function (tx) {
   tx.executeSql('CREATE TABLE IF NOT EXISTS PARTIES (name, description)');
 });
 
 function set_value(value){
-  document.getElementsByClassName('lista')[0].innerHTML += "<li>" + value + "</li>";
+  document.getElementsByClassName('lista')[0].innerHTML += "<li class='collection-item'>" + value + "</li>";
 }
 
 function get_value_from_input(classNameN, classNameD){
   var name        = document.getElementsByClassName(classNameN)[0].value;
   var description = document.getElementsByClassName(classNameD)[0].value;
   set_value_to_db(name, description);
-  //get_value_from_schedule();
+  //get_value_from_parties();
 }
 
 function set_value_to_db(name, description){
@@ -21,8 +22,11 @@ function set_value_to_db(name, description){
   });
 }
 
-function get_value_from_schedule(){
-  document.getElementsByClassName('lista')[0].innerHTML = null;
+function get_value_from_parties(){
+  var listDOM = document.getElementsByClassName('lista')[0]
+  if (listDOM){
+    listDOM.innerHTML = null;
+  }
 
   db.transaction(function (tx) {
     tx.executeSql('SELECT * FROM PARTIES', [], function(tx, results){
